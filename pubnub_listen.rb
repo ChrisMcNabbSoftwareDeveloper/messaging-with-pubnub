a681fa6c00c5cb5a18dc3f8505847afc1a9ce5ad
Chris McNabb <chrismcnabbsoftwaredeveloper@gmail.com>
2012-09-10 18:23:59 -0400

    added configuration to listener

--- pubnub_listen.rb
+++ pubnub_listen.rb
@@ -1,9 +1,13 @@
 require 'Pubnub'
+require 'configuration'
+require_relative 'config/pubnub_config'
 
-publish_key = "pub-91cb1068-b33c-4a39-9ba5-0b0097dc78e8"
-subscribe_key = "sub-6dea4ed9-f1f9-11e1-ad37-39ead1f66571"
-secret_key = "sec-YTkzMjJlOTctMDQxNS00ZDEyLTkyMDItOWMyOWVlNjM5MjVm"
-cipher_key = ""
+config = Configuration.for('messaging_with_pubnub')
+
+publish_key = config.pn_publish_key  
+subscribe_key = config.pn_subscribe_key 
+secret_key = config.pn_secret_key  
+cipher_key = config.pn_cipher_key
 ssl_on = false
 
 pubnub = Pubnub.new( publish_key, subscribe_key, secret_key, cipher_key, ssl_on )
